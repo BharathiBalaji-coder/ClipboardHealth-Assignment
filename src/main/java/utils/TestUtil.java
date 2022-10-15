bfad10b40daa8099157994c75a115b7caae31a01
Bharathi Balaji Rangaraj <rangarajbharathibalaji@gmail.com>
2022-10-15 22:08:22 +0530

    commit test util class

--- src/main/java/utils/TestUtil.java
+++ src/main/java/utils/TestUtil.java
@@ -14,7 +14,7 @@ public class TestUtil extends TestBase {
     public static long PAGE_LOAD_TIMEOUT = 30;
     public static long IMPLICIT_WAIT = 30;
 
-    public static void takeScreenshotAtEndOfTest() throws IOException {
+        public static void takeScreenshotAtEndOfTest() throws IOException {
         File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         String currentDir = System.getProperty("user.dir");
         FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
